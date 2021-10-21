218351f366aae496105553e2a482bd46f73416fb
e1b18073 <e1b18073@st.oit.ac.jp>
2021-10-21 17:37:57 +0900

    ユーザ情報を取得して表示

--- src/main/java/oit/is/z1652/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1652/kaizi/janken/controller/Lec02Controller.java
@@ -1,29 +1,35 @@
 package oit.is.z1652.kaizi.janken.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import oit.is.z1652.kaizi.janken.model.Janken;
-import oit.is.z1652.kaizi.janken.model.Entry;
+import oit.is.z1652.kaizi.janken.model.*;
 
 @Controller
 public class Lec02Controller {
 
   @Autowired
-  private Entry entry;
+  UserMapper userMapper;
 
   @GetMapping("/lec02")
+  @Transactional
   public String lec02(Principal prin, ModelMap model) {
     String loginUser = prin.getName();
-    this.entry.addUser(loginUser);
-    model.addAttribute("entry", this.entry);
+    User user = new User();
+    user.setName(loginUser);
+    userMapper.insertUser(user);
+    ArrayList<User> users = userMapper.selectAllUsers();
+    model.addAttribute("users", users);
     return "lec02.html";
   }
 
--- src/main/java/oit/is/z1652/kaizi/janken/model/UserMapper.java
+++ src/main/java/oit/is/z1652/kaizi/janken/model/UserMapper.java
@@ -1,5 +1,20 @@
 package oit.is.z1652.kaizi.janken.model;
 
-public class UserMapper {
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+  @Select("SELECT id,name from users")
+  ArrayList<User> selectAllUsers();
+
+  @Insert("INSERT INTO users (name) VALUES (#{name});")
+  @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+  void insertUser(User users);
 
 }
